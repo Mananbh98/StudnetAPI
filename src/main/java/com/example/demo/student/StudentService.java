b9e9d4c974a2384aef8670d68c8eb639bfb3daed
Mananbh98 <mananbhanushali98@gmail.com>
2021-01-15 18:08:00 +0530

    Add JpaRepository interface and connecting it to StudentService in student-api V1.0.6

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
 
@@ -12,8 +13,20 @@ import java.util.List;
 @Service  //This class is ment to be service class looking at the name
 public class StudentService {
 
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository){
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents(){
-        return List.of(
+        return  studentRepository.findAll();
+    }
+}
+
+/* Static data
+* List.of(
                 new Student(
                         1L,
                         "Manan",
@@ -28,5 +41,4 @@ public class StudentService {
                         LocalDate.of(2001, Month.MAY, 5),
                         20 )
         );
-    }
-}
+    }*/
