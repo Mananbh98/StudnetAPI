649bf6ab4476fdbf563363e0870a0809ddeb1244
Mananbh98 <mananbhanushali98@gmail.com>
2021-01-15 13:10:12 +0530

    Add the api layer to student-api V1.0.1 with new mapping

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,39 +1,14 @@
 package com.example.demo;
 
-import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.List;
 
 @SpringBootApplication
-@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-	@GetMapping
-	public List<Student> hello(){
-		return List.of(
-				new Student(
-						1L,
-						"Manan",
-						"manan.bhanu@gamil.com",
-						LocalDate.of(2000, Month.MAY, 5),
-						21
-				),
-		new Student(
-				2L,
-				"Gopi",
-				"gopi.bhanu@gamil.com",
-				LocalDate.of(2001, Month.MAY, 5),
-				20 )
-		);
-	}
+
 
 }
